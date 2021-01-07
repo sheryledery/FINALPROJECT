6701723ffc5a0298fd36475bf03136594805cf36
MacBook Pro <macbookpro@sheryl-mac.lan>
2021-01-07 12:57:11 +0200

    update

--- ablog/settings.py
+++ ablog/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'p8rt608%qn==+(g=shhl2+7csd4@n*_x*w6ultujl6wft24soe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "sheryledery.pythonanywhere.com",
+    "127.0.0.1",
+]
 
 
 # Application definition
